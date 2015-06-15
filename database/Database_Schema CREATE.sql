a8ff243d0f17109fdaca48de3f115682b89a18c1
LarsGH <lars.syfuss@googlemail.com>
2015-06-15 20:49:15 +0200

    Adjusted script to recreate all tables

--- database/Database_Schema CREATE.sql
+++ database/Database_Schema CREATE.sql
@@ -2,17 +2,20 @@
 
 /* CREATE EXTENSION postgis; */
 
+DROP TABLE IF EXISTS devices CASCADE;
 CREATE TABLE IF NOT EXISTS devices (
 	device_id varchar(20) PRIMARY KEY,
 	theft_protection_active boolean NOT NULL
 );
 
+DROP TABLE IF EXISTS geofences CASCADE;
 CREATE TABLE geofences (
 	geofence_id SERIAL PRIMARY KEY,
 	device_id varchar(20) NOT NULL REFERENCES devices(device_id) ON UPDATE CASCADE ON DELETE CASCADE,
 	area geometry
 );
 
+DROP TABLE IF EXISTS messages CASCADE;
 CREATE TABLE messages (
 	message_id SERIAL PRIMARY KEY,
 	device_id varchar(20) NOT NULL REFERENCES devices(device_id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -21,6 +24,7 @@ CREATE TABLE messages (
 	"time" timestamp without time zone
 );
 
+DROP TABLE IF EXISTS iobdata CASCADE;
 CREATE TABLE iobdata (
     id SERIAL PRIMARY KEY,
     ip text,
