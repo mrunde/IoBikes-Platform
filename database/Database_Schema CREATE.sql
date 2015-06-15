a86ae559f041dbabf7a90cfd049ee0c43e702a02
LarsGH <lars.syfuss@googlemail.com>
2015-06-15 16:03:53 +0200

    CREATE statements for the database

---
+++ database/Database_Schema CREATE.sql
@@ -0,0 +1,28 @@
+﻿/* CREATE DATABASE iob; */
+
+/* CREATE EXTENSION postgis; */
+
+CREATE TABLE IF NOT EXISTS devices (
+	device_id varchar(20) PRIMARY KEY,
+	theft_protection_active boolean NOT NULL
+);
+
+CREATE TABLE geofences (
+	geofence_id SERIAL PRIMARY KEY,
+	device_id varchar(20) NOT NULL REFERENCES devices(device_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	area geometry
+);
+
+CREATE TABLE messages (
+	message_id SERIAL PRIMARY KEY,
+	device_id varchar(20) NOT NULL REFERENCES devices(device_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	loc geometry,
+	"time" timestamp without time zone,
+	"data" varchar(20) NOT NULL
+);
+
+CREATE TABLE iobdata (
+    id SERIAL PRIMARY KEY,
+    ip text,
+    data jsonb
+);
