0666f86f2c8ca6d0b3043a4f6d7ce905b60dfda8
Daniel <daniel.sawatzky@uni-muenster.de>
2015-06-25 21:06:09 +0200

    Updated DB schema SQL file

---
+++ database/db_schema_create_v2.sql
@@ -0,0 +1,25 @@
+﻿/* CREATE DATABASE iob; */
+
+/* CREATE EXTENSION postgis; */
+
+CREATE TABLE IF NOT EXISTS devices (
+	device_id varchar(20) PRIMARY KEY,
+	theft_protection_active boolean NOT NULL
+);
+
+CREATE TABLE geofences (
+	geofence_id SERIAL PRIMARY KEY,
+	device_id varchar(20) NOT NULL REFERENCES devices(device_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	lat double precision,
+	lon double precision,
+	radius double precision
+);
+
+CREATE TABLE messages (
+	message_id SERIAL PRIMARY KEY,
+	device_id varchar(20) NOT NULL REFERENCES devices(device_id) ON UPDATE CASCADE ON DELETE CASCADE,
+	lat double precision,
+	lon double precision,
+	"time" timestamp without time zone,
+	temp double precision
+);
